77a78983e417b28c5369a3835d94edddbca1a2ea
Mckarl Manuel <manmckarl@gmail.com>
2018-07-27 11:02:31 -1000

    products db/routes function properly on postman, still needs handlebars

--- db/products.js
+++ db/products.js
@@ -1,4 +1,4 @@
-const products = [{ name: 'Cake', price: 2, inventory: 10, id: 1  }];
+const products = [{ name: 'Cake', price: 2, inventory: 10, id: 1 }];
 let currentId = 1;
 
 function all() {
@@ -18,14 +18,37 @@ function getProduct(id) {
     }
   }
 }
-// add more methods here
 
-function editItem(product){
-
+function editProduct(id, productInfo){
+  num = parseFloat(id.slice(1, id.length));
+  for (let i=0; i<products.length; i++){
+    if (typeof num === 'number' && products[i].id === num){
+      products[i].name = productInfo.name;
+      products[i].price = productInfo.price;
+      products[i].inventory = productInfo.inventory;
+      return products[i];
+    }
+  }
 }
+
+function deleteProduct(id){
+  num = parseFloat(id.slice(1, id.length));
+  for (let i=0; i<products.length; i++){
+    if (typeof num === 'number' && products[i].id === num){
+      products.splice(i, 1);
+      return products;
+    }
+  }
+}
+
+
+
+// add more methods here
 
 module.exports = {
   all: all,
   addProduct: addProduct,
   getProduct: getProduct,
+  editProduct: editProduct,
+  deleteProduct: deleteProduct
 }
--- routes/products.js
+++ routes/products.js
@@ -1,28 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const productsDb = require('../db/products');
-let num = Number;
 
 router.route('/');
+// Please remember to fix so it renders
+// remember to implement some sort of res.redirect when appropriate
 
-router.get('/', (req, res)=>{
-  //console.log(req)
+// GETS ENTIRE CATALOG OF PRODUCTS AND INFORMATION
+router.get('/', (req, res) => {
   res.send(productsDb.all());
 });
 
-router.get('/*', (req, res)=>{
+// GETS ONE SPECIFIC PRODUCT'S INFORMATION
+// IF IT FAILS, IT RETURNS ENTIRE CATALOG
+router.get('/*', (req, res) => {
   let productId = parseFloat(req.url.slice(1, req.url.length));
-  if (productId){
-    res.send(productsDb.getProduct(req.url))
+  if (productId) {
+    res.send(productsDb.getProduct(req.url));
   } else {
-    res.send(productsDb.all())
-  }
-})
+    res.send(productsDb.all());
+  };
+});
 
-router.post('/', (req, res)=>{
-  const reqBody = req.body;
-  productsDb.addProduct(reqBody);
-  res.send(productsDb.all())
+// ADDS A PRODUCT+INFORMATION TO THE CATALOG
+// BRINGS UP ENTIRE CATALOG AFTER ADDING A PRODUCT
+router.post('/', (req, res) => {
+  const productBody = req.body;
+  console.log(req.body);
+  console.log(productBody.name);
+  console.log(productBody.price);
+  console.log(productBody.inventory);
+  productsDb.addProduct(productBody);
+  res.send(productsDb.all());
+});
+
+// FINDS A SPECIFIC PRODUCT AND EDITS THE CONTENTS
+router.put('/*', (req, res) => {
+  const productBody = req.body;
+  let productId = parseFloat(req.url.slice(1, req.url.length));
+  if (productId) {
+    res.send(productsDb.editProduct(req.url, productBody));
+  };
+});
+
+// FINDS A SPECIFIC PRODUCT AND DELETES IT FROM THE ARRAY
+router.delete('/*', (req, res)=>{
+  let productId = parseFloat(req.url.slice(1, req.url.length));
+  if (productId) {
+    res.send(productsDb.deleteProduct(req.url));
+  };
 });
 
 module.exports = router;
